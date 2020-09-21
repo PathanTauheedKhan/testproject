0fd8753c0566c4e41d3bae39e232a1a8433d887b
Tauheed Khan <Tauheed Khan@LAPTOP-MS9PU6DU>
2020-09-21 14:16:20 +0530

    added Test1.java class

---
+++ testproject/src/testproject/Test1.java
@@ -0,0 +1,11 @@
+package testproject;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the first test programm");
+
+	}
+
+}
