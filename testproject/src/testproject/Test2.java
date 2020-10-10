286cdda9e5fec89d811fc8cc2fe8e89b35493ae7
Tauheed Khan <Tauheed Khan@LAPTOP-MS9PU6DU>
2020-10-11 00:05:38 +0530

    adding two numbers

--- testproject/src/testproject/Test2.java
+++ testproject/src/testproject/Test2.java
@@ -7,11 +7,13 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a number");
+		System.out.println("Enter the numbers");
 		int a = sc.nextInt();
-		System.out.println("The Entered number is "+a);
+		int b = sc.nextInt();
 		
+		System.out.println("The sum of Entered number is "+(a+b));
 		
+	
 		sc.close();
 		
 		
