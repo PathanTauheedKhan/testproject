ed0354e5b95b9957e9fa2c368cd9330e83a273db
Tauheed Khan <Tauheed Khan@LAPTOP-MS9PU6DU>
2020-10-10 23:53:56 +0530

    added a new statement

--- testproject/src/testproject/Test1.java
+++ testproject/src/testproject/Test1.java
@@ -5,7 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is the first test programm");
-
+		System.out.println("Hello world");
 	}
 
 }
