93953d6fba33de084f40bb20aebb71e85d7c63b4
Tauheed Khan <Tauheed Khan@LAPTOP-MS9PU6DU>
2020-10-11 00:02:55 +0530

    Test2 file is added

---
+++ testproject/src/testproject/Test2.java
@@ -0,0 +1,21 @@
+
+package testproject;
+import java.util.*;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int a = sc.nextInt();
+		System.out.println("The Entered number is "+a);
+		
+		
+		sc.close();
+		
+		
+
+	}
+
+}
